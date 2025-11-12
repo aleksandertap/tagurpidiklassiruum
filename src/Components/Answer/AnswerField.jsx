a19bcfe1d94eb234dc7a10dfa25187ad71b15190
Paan <paan.kaur@gmail.com>
2025-11-12 16:02:05 +0200

    Mängu ala komponent: sisestuse ala(input field)

---
+++ src/Components/Answer/AnswerField.jsx
@@ -0,0 +1,23 @@
+import LetterBox from "@/src/Components/Answer/LetterBox.jsx";
+import { View } from "react-native";
+
+/**
+ * Component to display the answer field with letter boxes.
+ *  {string} attemptWord - The word attempted by the user.
+ *  {string} correctWord - The correct word to compare against.
+ */
+
+export default function AnswerField({ attemptWord = "", correctWord = "" }) {
+  return (
+    <View className="p-4 mx-4 flex flex-row justify-center gap-1">
+      {/* Renders letter boxes for each character in the correctWord */}
+      {correctWord.split("").map((_, i) => (
+        <LetterBox
+          key={i}
+          text={(attemptWord.charAt(i) || "").toUpperCase()}
+          bgColor="bg-[#E8E8E8]"
+        />
+      ))}
+    </View>
+  );
+}
---
+++ src/Components/Answer/LetterBox.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { Pressable, Text } from "react-native";
+
+/**
+ * A single letter box component that can hide or show its letter on press.
+ *  {string} bgColor - The background color class for the letter box.
+ *  {function} onPress - The function to call when the box is pressed.
+ *  {string} text - The letter to display in the box.
+ *  {boolean} setIsVisible - Optional prop to control visibility from parent.
+ */
+
+export default function LetterBox({ bgColor = "bg-[#E8E8E8]", onPress, text }) {
+  const [isTextVisible, setIsTextVisible] = useState(true);
+
+
+  return (
+    <Pressable
+      className={`w-[50px] h-[50px] ${bgColor} flex items-center justify-center rounded-md`}
+      onPress={onPress}
+    >
+      {!isTextVisible && (
+        <Text className="text-3xl font-semibold text-black text-center leading-[50px]">
+          {String(text || " ")
+            .toUpperCase()
+            .charAt(0)}
+        </Text>
+      )}
+    </Pressable>
+  );
+}
