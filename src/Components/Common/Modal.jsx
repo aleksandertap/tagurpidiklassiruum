3d839e90436b98f29159d5812a0ddc2fe0c268d5
aleksandertap <aleksander.tapsi@voco.ee>
2025-11-12 12:17:56 +0200

    made modal component somewhat. height is funky?

---
+++ src/Components/Common/Modal.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { FlatList, View, Text } from "react-native";
+import { Button } from "./Button";
+
+
+const Modal = ({ showHistory, showGuide, wrongGuesses, data }) => {
+  const historyData = data.filter((item) => item.active === false);
+  const correctCount = historyData.length;
+
+  const renderNotActiveItem = ({ item }) => (
+    <View className="mb-5 text-center">
+      <View className="flex-row justify-center items-center gap-2">
+        <View className="w-full h-px bg-black " />
+        <Text className="text-2xl">{item.word.charAt(0).toUpperCase() + item.word.slice(1)}</Text>
+        <View className="w-full h-px bg-black " />
+      </View>
+      <Text className="text-base">{item.definition}</Text>
+    </View>
+  );
+
+  return (
+    
+      <View className="p-6 bg-white rounded-lg shadow-lg justify-between items-center w-4/5">
+        {showGuide ? (
+          <View className="mb-4 items-center">
+            <Text className="text-2xl font-bold">Tagurpidi klassiruum</Text>
+            <Text className="text-base mb-4">Õpi eesti keele sõnu mänguliselt</Text>
+            <View className="gap-2">
+              <Text className="text-2xl">* Ekraanil on sõna seletus</Text>
+              <Text className="text-2xl">* Kirjuta õige sõna ja vajuta "OK"</Text>
+              <Text className="text-2xl">* Ajalugu asub üleval paremal</Text>
+              <Text className="text-2xl">* Juhendit saab taas kuvada üleval vasakul</Text>
+            </View>
+          </View>
+        ) : null}
+        {showHistory ? (
+          <View className="mb-4 items-center">
+            <Text className="text-2xl">Õigeid vastuseid</Text>
+            <Text className="text-2xl font-bold">{correctCount}</Text>
+            <Text className="text-2xl">Valesid vastuseid</Text>
+            <Text className="text-2xl font-bold">{wrongGuesses}</Text>
+            <View className="w-full h-80"> 
+            <FlatList 
+              data={historyData} 
+              renderItem={renderNotActiveItem}
+              keyExtractor={(item, index) => item.word + index}
+              contentContainerStyle={{ width: '100%' }}
+              showsVerticalScrollIndicator={false}
+            />
+          </View>
+          </View>
+        ) : null}
+        <Button style="normal" title="Sulge" onPress={() => console.log("sulge pressed")} />
+      </View>
+  
+  );
+};
+
+export default Modal;
