7100abbc27e33dd5bc676d8178e0dccda5355f4d
aleksandertap <aleksander.tapsi@voco.ee>
2025-11-13 10:23:01 +0200

    initalcommitfortesting

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import Button from "@/src/Components/Common/Button";
+import {Button} from "@/src/Components/Common/Button";
 import { Text, View } from "react-native";
 import "./global.css";
 
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
+        Welcome to Nativewind!yeaah
       </Text>
 
       <Button
